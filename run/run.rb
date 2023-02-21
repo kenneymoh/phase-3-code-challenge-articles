a7110d9163c83d0ebe59fbb3ee11ae0fcb7d12ef
kennedy <kenneyrit@gmail.com>
2023-02-21 09:01:12 +0300

    created and updated the run file

---
+++ run/run.rb
@@ -0,0 +1,13 @@
+
+author1 = Author.new("Kenney")
+author2 = Author.new("mikel arteta")
+author3 = Author.new("kenney rodgers")
+
+magazine1 = Magazine.new("Standard", "news")
+magazine2 = Magazine.new("manjesta", "transport")
+magazine3 = Magazine.new("crime", "informative")
+
+article1 = Article.new(author1, "magazine1", "sports")
+article2 = Article.new(author1, "magazine2", "sleep")
+article3 = Article.new(author2, "magazine1", "comedy")
+article3 = Article.new(author3, "magazine3", "huzuni")
