6a5d62c0ceddcd78bde248186631145577f7018d
kennedy <kenneyrit@gmail.com>
2023-02-22 12:42:30 +0300

    updated run file

--- run/run.rb
+++ run/run.rb
@@ -1,8 +1,9 @@
-
+require_relative '../config/environment.rb'
+require 'pry'
 author1 = Author.new("Kenney")
-author2 = Author.new("mikel arteta")
-author3 = Author.new("kenney rodgers")
-author4 = Author.new("mikel rodgers")
+author2 = Author.new("Mikel arteta")
+author3 = Author.new("Kenney rodgers")
+author4 = Author.new("Mikel rodgers")
 
 magazine1 = Magazine.new("Standard", "news")
 magazine2 = Magazine.new("manjesta", "transport")
@@ -13,3 +14,6 @@ article1 = Article.new(author1, "magazine1", "sports")
 article2 = Article.new(author1, "magazine2", "sleep")
 article3 = Article.new(author2, "magazine1", "comedy")
 article4 = Article.new(author3, "magazine3", "huzuni")
+binding.pry
+
+0
